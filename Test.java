54ef84b43d96f4b87b77cf0e7f502155a7c1fda8
VJ <xyz@gmail.com>
2024-08-20 10:02:36 +0530

    added java file

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class Test {
+
+    static void add()
+    {
+        int a=5;
+        int b=10;
+        int res=a+b;
+        System.out.println("the result is " +res);
+    }
+    public static void main(String[] args)
+    {
+        add();
+    }
+}
+
