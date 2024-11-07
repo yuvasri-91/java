f9b3fd48a770642cfcff10947a56b24a145ded95
shreyaindoria <144434923+shreyaindoria@users.noreply.github.com>
2024-11-07 17:26:15 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -4,7 +4,7 @@ public class Test {
     {
         int a=5;
         int b=10;
-        int c=5;
+        int c=20;
         int res=a+b+c;
         System.out.println("the result is " +res);
     }
